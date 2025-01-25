fdeb6f662359f41025a18c3e595dda7074fcc9fc
SerhiiBruj <sergijk174@gmail.com>
2025-01-26 00:41:23 +0200

    attemtp to fix render bugs 2

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -203,7 +203,7 @@ impl Game {
         for tree in trees {
             let distance = ((self.player_x - tree.0).powi(2) + (self.player_y - tree.1).powi(2)).sqrt();
             let player_radius =10.0;
-            let tree_radius = 12.0;
+            let tree_radius = 10.0;
             if distance < player_radius + tree_radius {
                 return true; 
             }
